d32e59c9dd66925effa81a4f2373585cf7b7ba40
manuelhe123 <60900141+manuelhe123@users.noreply.github.com>
2020-02-10 21:59:39 +0100

    Actividad 3 eje 13

---
+++ act3eje13.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class act3eje13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        for(int i=1;i<=5;i++)
+	        {
+	            for( int a=0;a<=10;a++)
+	            {
+	                System.out.println(i + " x " + a + " = " + i*a);
+	            }
+	            System.out.println();
+	        }}}
